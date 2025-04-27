6861a54e8a34d8a839da6517dc5bb2bf4d8a740b
choragenikita-08 <choragenikita@gmail.com>
2025-04-27 17:37:48 +0530

    cleared unnecessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,20 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { BrowserModule, platformBrowser } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
 import { LoginComponent } from './Auth/login/login.component';
 import { RegisterComponent } from './Auth/register/register.component';
-import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatError } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
 import { ToastrModule } from 'ngx-toastr';
 import { AuthModule } from '@angular/fire/auth';
-import { AuthService } from './Auth/auth.service';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    // AppComponent,
-    LoginComponent,
-    RegisterComponent,
-  ],
+  declarations: [LoginComponent, RegisterComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
@@ -31,11 +22,8 @@ import { SharedModule } from './shared/shared.module';
     CommonModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    MatCardModule, // Angular Material modules
     MatError,
     MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
     AuthModule,
     SharedModule,
     ToastrModule.forRoot({
@@ -45,15 +33,8 @@ import { SharedModule } from './shared/shared.module';
       closeButton: true, // Show close button
       progressBar: true, // Show a progress bar
     }),
-    // ToastrModule.forRoot({ // Config options for Toastr
-    //   timeOut: 3000,
-    //   positionClass: 'toast-top-right',
-    //   preventDuplicates: true,
-    // }),
   ],
-  providers: [
-    // AuthService
-  ],
+  providers: [],
   exports: [],
   bootstrap: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
